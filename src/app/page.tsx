110f47dbeb242714a48d2b06dba27372ccae40e3
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2024-07-23 19:07:06 -0500

    header component init integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import { Button } from '@nextui-org/react';
+import { signIn, signOut } from '@/actions';
+import { auth } from '@/auth';
 
-export default function Home() {
+export default async function Home() {
+	const session = await auth();
 	return (
 		<div>
-			<Button>Click Me</Button>
+			<form action={signIn}>
+				<Button type='submit'>SignIn</Button>
+			</form>
+			<form action={signOut}>
+				<Button type='submit'>SignOut</Button>
+			</form>
+			{JSON.stringify(session)}
 		</div>
 	);
 }
