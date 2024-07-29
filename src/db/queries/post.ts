87b2327c98d9ff4016fd937e109ad56f112c449f
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2024-07-29 14:12:07 -0500

    render list of posts

---
+++ src/db/queries/post.ts
@@ -0,0 +1,21 @@
+import type { Post } from '@prisma/client';
+import { db } from '@/db';
+
+export type PostWithData = Post & {
+	topic: { slug: string };
+	_count: { comments: number };
+	user: { name: string | null };
+};
+
+export const fetchPostsByTopicSlug = (
+	slug: string
+): Promise<PostWithData[]> => {
+	return db.post.findMany({
+		where: { topic: { slug } },
+		include: {
+			topic: { select: { slug: true } },
+			user: { select: { name: true } },
+			_count: { select: { comments: true } },
+		},
+	});
+};
